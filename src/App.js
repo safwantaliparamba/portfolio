59b69e2993c9771f69c583420faad62b055650f1
safwantaliparamba <safwansafu242090@gmail.com>
2022-05-28 17:43:59 +0530

    router fully added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from "./screens/Home";
 import NotFound from "./screens/NotFound";
 import Portfolio from "./screens/Portfolio";
 import Header from './components/Header'
+import Contact from "./screens/Contact";
 
 function App() {
     return (
@@ -15,6 +16,7 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/portfolio" element={<Portfolio />} />
+                <Route path="/contact" element={<Contact />} />
                 <Route path="*" element={<NotFound />} />
             </Routes>
         </Router>
